c95f41dd86d6f29a22d82340add8c3787c95396c
Barkie <marat.kaz@gmail.com>
2016-05-30 13:09:58 +0300

    removed_old_comments

--- interface_vlan_list_test.py
+++ interface_vlan_list_test.py
@@ -4,12 +4,6 @@ import os
 from os import listdir
 import xlrd
 import xlwt
-#open file
-#f = open('/samba/allaccess/1.txt', 'r')
-#read file
-#str = f.read()
-#counter
-
 
 listFF = listdir('C:/python/configs/')
 #create excel workbook with write permissions (xlwt module)
@@ -38,123 +32,74 @@ ws.write(0, 7, 'summary(testing purpose)')
 i = 1
 
 def search():
+    #w/o it countet don't work inside function
     global i
-#    #search OS
-#    OSname = re.search(r"Operating System:.*?([\S\s].*)\s", str).group(1)
-#    #search SP
-#    SPname = re.search(r"Service Pack:.*?([\S\s].*)\s", str).group(1)
-#    #combine OS + " " + SP like "Windows XP Service Pack 1"
-#    OS_SP = OSname + " " + SPname
-#    #search domain
-#    domain = re.search(r"Name:.*?([\S\s].*)\s", str).group(1)
-#    #search network name
-#    netbios = re.search(r"The network name.*:.*?([\S\s].*)\s", str).group(1)
-    #search_hostname
+    #trying to find hostname of the device
     hostname = re.findall(r"\nhostname.([\S\s].*)\n", some_str)
+    #if some1 is found
     if hostname != []:
         #remove_duplicates
         hostname_no_duplic = list(set(hostname))
         hostname_str = ' '.join(hostname_no_duplic)
+        #write to the excel
         ws.write(i, 1, hostname_str)
-    #search IP address Cisco interface vlan
-    #trying to find loopback0 w/o descr
-    #lo0 = re.findall(r" (Loopback0)[\s\S]{1,80}(ip address.*)\s*.*\ninterface", str)
-    #if lo0 != []:
-    #    lo0_str = ' '.join(lo0)
-    #    ws.write(i, 2, lo0_str)
-    #    i += 1
-    #
+    #trying to find all interfaces with IP addresses
     vlanif = re.findall(r"\ninterface ((?:Loopback.*|Tunnel.*|GigabitEthernet.*|Vlan.*|Fast.*\n|Serial.*)[^\!]*ip address (?:[0-9]{1,3}\.){3}[0-9]{1,3} (?:[0-9]{1,3}\.){3}[0-9]{1,3}\s)?", some_str)
+    #removing duplicates
     vlanif_no_duplic = list(set(vlanif))
+    #sorting interfaces alphabetically
     vlanif_no_duplic.sort()
+    #if some interfaces were found
     if vlanif_no_duplic != []:
- #    vlanif_str = list(set(vlanif))
-        #combo-wombo list to string   
-        #vlanif_str = ' '.join(vlanif)
-        #write to excel
-        #vlanif_list = re.findall(r"(interface Loopback.*|interface Tunnel.*|interface GigabitEthernet.*|interface Vlan.*)\s*.*(nameif.*|description.*)[\s\S]{0,115}(ip address.*)\s", vlanif_str)
+        #for every interface run new searches
         for item in vlanif_no_duplic:
+            #combo-wombo list to string
             item_str = ''.join(item)
+            #trying to find interface type and name 
             interface = re.findall(r"(Loopback\S*|Tunnel\S*|GigabitEthernet\S*|Vlan\S*|Fast\S*|Serial\S*)\s", item_str)
+            #remove duplicates
             interface_no_duplic = list(set(interface))
-            if interface_no_duplic != []:
+            #if something is found then convert to string and write to excel
+            if interface_no_duplic != []:            	
                 interface_str = ''.join(interface_no_duplic)
                 ws.write(i, 2, interface_str)
+            #trying to find Cisco ASA interface description (nameif)
             interface_asa = re.findall(r"([\S\s]*)\nnameif", item_str)
-            if interface_asa != []:
-                interface_asa_str = ''.join(interface_asa)
+            #if something is found then convert to string and write to excel
+            if interface_asa != []:            	
+                interface_asa_str = ''.join(interface_asa)                
                 ws.write(i, 2, interface_asa_str)
+            #trying to find Cisco switch/router/industrial switch interface description
             descr = re.findall(r"description ([\S\s]*?)\n", item_str)
+            #if something is found then convert to string and write to excel
             if descr != []:
                 descr_str = ''.join(descr)
                 ws.write(i, 3, descr_str)
+            #ne pomnu zachem eto pisal, nado budet potom razobratsya)
             nameif = re.findall(r"nameif ([\S\s]*?)\n", item_str)
+            #if something is found then convert to string and write to excel
             if nameif != []:
                 nameif_str = ''.join(nameif)
                 ws.write(i, 3, nameif_str)
+            #trying to find IP address terminated on the interface
             ip = re.findall(r"ip address ((?:[0-9]{1,3}\.){3}[0-9]{1,3} (?:[0-9]{1,3}\.){3}[0-9]{1,3})\s?$", item_str)
+            #if something is found then convert to string and write to excel
             if ip != []:
                 ip_str = ''.join(ip)
                 ws.write(i, 4, ip_str)
+            #trying to find secondary IP address terminated on the interface
             ip_sec = re.findall(r"ip address ((?:[0-9]{1,3}\.){3}[0-9]{1,3} (?:[0-9]{1,3}\.){3}[0-9]{1,3}) secondary", item_str)
             if ip_sec != []:
                 ip_sec_str = ''.join(ip_sec)
                 ip_secondary_str = ip_sec_str + ' secondary'
+                #counter +1
                 i += 1
                 ws.write(i, 4, ip_secondary_str)
+            #debug thing, write raw interface string found in another excel column, for debug purposes
             ws.write(i, 7, item_str)
+            #counter +1
             i += 1
-#        ws.write(i, 2, vlanif_str)
-    #search IP address MOXA
-#    ipaddress_moxa = re.findall(r"IPAddress.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s", str)
-#    if ipaddress_moxa != []:
-#        ipmoxa_str = ' '.join(ipaddress_moxa)
-#        ws.write(i, 3, ipmoxa_str)
-    #search software_version
-#    soft = re.findall(r"flash:\/{1,2}([\S\s].*).bin", str)
-#    if soft != []:
-#        soft_str = ' '.join(soft)
-#        ws.write(i, 4, soft_str)
-    #search cdp_nei
-#    cdp = re.findall(r"Platform  Port ID([\S\s]*?)#", str)
-#    if cdp != []:
-#        cdp_str = ' '.join(cdp)
-#        ws.write(i, 5, cdp_str)
-    #search loopback
-    #lpb = re.findall(r"(Loopback.*)\n*.*\n*.ip.*?((?:[0-9]{1,3}\.){3}[0-9]{1,3})", str)
-    #if lpb != []:
-    #    lpb_str = ' '.join(lpb)
-    #    ws.write(i, 6, lpb_str)
-   
 
-#    #combine network name + " " + IP address
-#    netbios_ip = netbios + " " + ipaddress
-#    #search Windows Installer software
-#    software_list1 = re.search(r"installation date([\S\s]*)Installed software", str).group(1)
-#    #search Registry software
-#    software_list2 = re.search(r"Title version([\S\s]*)Keys installed software", str).group(1)
-#    #combine 2 software lists into 1
-#    software_list = software_list1 + " " + software_list2
-#    #search printer list
-#    printers = re.search(r"name Port([\S\s]*)Regional settings", str).group(1)
-#    #search server manufacturer + title (HP,Dell,Vmware etc)
-#    server_man = re.search(r"Manufacturer: ([\S\s]*)Serial .umber", str).group(1)
-#    #delete Title:, Name: from manufacturer
-#    try:
-#      server_man = server_man.replace('Title:','')
-#    except:
-#      pass
-#    try:
-#      server_man = server_man.replace('Name:','')
-#    except:
-#      pass
-    #write information to Excel shells
-#    ws.write(i, 3, OS_SP)
-#    ws.write(i, 2, netbios_ip)
-#    ws.write(i, 4, software_list)
-#    ws.write(i, 5, domain)
-#    ws.write(i, 8, printers)
-#    ws.write(i, 1, server_man)	
     print('info saved')
     #counter+1 to next excel line
     i += 1
